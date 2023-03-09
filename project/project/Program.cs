0f663a05546d177076debdc3c77433e4946c8906
Alireza Saei <saeialireza359@gmail.com>
2023-03-09 17:57:43 +0330

    fixed infinte loop message publish

--- project/project/Program.cs
+++ project/project/Program.cs
@@ -10,15 +10,17 @@ namespace MqttServerExample
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             MainAsync(args).GetAwaiter().GetResult();
         }
         
-        static async Task MainAsync(string[] args)
+        public static async Task MainAsync(string[] args)
         {
             var options = new MqttServerOptionsBuilder()
+                //.WithEndpoint("192.1")
                 .WithDefaultEndpoint()
+                //.WithEndpointPort(1884)
                 .WithDefaultEndpointPort(1883)
                 .Build();
 
@@ -31,20 +33,22 @@ namespace MqttServerExample
             {
                 Console.WriteLine($"Client '{e.ClientId}' disconnected.");
             });
-            mqttServer.ApplicationMessageReceivedHandler = new MqttApplicationMessageReceivedHandlerDelegate(async e =>
+            mqttServer.ApplicationMessageReceivedHandler = new MqttApplicationMessageReceivedHandlerDelegate(e =>
             {
                 string messagePayload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                
                 dynamic messageJson = JsonConvert.DeserializeObject(messagePayload);
                 Console.WriteLine($"Received message: {messageJson}");
-
+                
                 // Publish message to all clients
-                var message = new MqttApplicationMessageBuilder()
-                    .WithTopic(e.ApplicationMessage.Topic)
-                    .WithPayload(Encoding.UTF8.GetBytes(messagePayload))
-                    .WithExactlyOnceQoS()
-                    .WithRetainFlag()
-                    .Build();
-                await mqttServer.PublishAsync(message, CancellationToken.None);
+                // var message = new MqttApplicationMessageBuilder()
+                //     .WithTopic(e.ApplicationMessage.Topic)
+                //     .WithPayload(Encoding.UTF8.GetBytes(messagePayload))
+                //     .WithExactlyOnceQoS()
+                //     //.WithAtLeastOnceQoS()
+                //     .WithRetainFlag()
+                //     .Build();
+                // await mqttServer.PublishAsync(message, CancellationToken.None);
             });
 
             await mqttServer.StartAsync(options);
