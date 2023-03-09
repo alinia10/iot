b677e0f1351a0dc1c7f04b330a58b0d4c6e16bfe
Alireza Saei <saeialireza359@gmail.com>
2023-03-09 11:04:23 +0330

    backend added

---
+++ project/project/Program.cs
@@ -0,0 +1,24 @@
+﻿using MQTTnet;
+using MQTTnet.Server;
+using System.Text;
+using static System.Console;
+using Npgsql;
+using project;
+
+class Project
+    {
+        public static void Main(string[] args)
+        {
+            PostgresConnection conn = new PostgresConnection("localhost", "5432", "mydatabase", "myusername", "mypassword");
+            conn.OpenConnection();
+            NpgsqlDataReader reader = conn.ExecuteQuery("SELECT * FROM history");
+            while (reader.Read())
+            {
+                Console.WriteLine($"Column 1: {reader.GetString(0)}, Column 2: {reader.GetString(1)}");
+            }
+        
+            conn.CloseConnection();
+            
+        }
+    }
+
